e978bb1af1fa0e8e24be044ec9b6db9a0167702b
maciejkula <noocrat@gmail.com>
2015-07-13 15:46:18 +0100

    Change tarball url.

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
     author='LYST Ltd (Maciej Kula)',
     author_email='data@lyst.com',
     url='https://github.com/lyst/rpforest',
-    download_url='https://github.com/lyst/rpforest/tarball/1.0',
+    download_url='https://github.com/lyst/rpforest/tarball/1.1',
     license='Apache 2.0',
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: Apache Software License'],
